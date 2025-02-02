29535e8d764ad6f75cec0263c82e601049770929
nazarshtiyuk <nazarshtiyuk@gmail.com>
2025-02-03 00:54:31 +0200

    added JSON function(encode)

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/NazarShtiyuk/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +18,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+		CreatedAt: time.Now(),
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year"`
+	Runtime   int32     `json:"runtime"`
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"`
+	CreatedAt time.Time `json:"created_at"`
+}
