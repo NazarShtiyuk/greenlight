67a64d6307eee1871a9784b9fad8b588c8e77ec8
nazarshtiyuk <nazarshtiyuk@gmail.com>
2025-02-03 01:04:52 +0200

    update movie field(runtime)

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,7 +6,7 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"`
+	Runtime   Runtime   `json:"runtime"`
 	Genres    []string  `json:"genres"`
 	Version   int32     `json:"version"`
 	CreatedAt time.Time `json:"created_at"`
