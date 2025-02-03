d9b95262c402403a214ede4a6e84884a19720b35
nazarshtiyuk <nazarshtiyuk@gmail.com>
2025-02-03 15:07:36 +0200

    update errors messages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -19,7 +18,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
